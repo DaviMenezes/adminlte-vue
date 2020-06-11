ef29a69bd673d8f2f1268fb3310362e0fa1c8f94
DaviMenezes <davimenezes.dev@gmail.com>
2020-06-11 12:25:04 -0300

    add dist path

---
+++ dist/build/js/AdminLTE.js
@@ -0,0 +1,23 @@
+import CardRefresh from './CardRefresh'
+import CardWidget from './CardWidget'
+import ControlSidebar from './ControlSidebar'
+import DirectChat from './DirectChat'
+import Dropdown from './Dropdown'
+import Layout from './Layout'
+import PushMenu from './PushMenu'
+import Toasts from './Toasts'
+import TodoList from './TodoList'
+import Treeview from './Treeview'
+
+export {
+  CardRefresh,
+  CardWidget,
+  ControlSidebar,
+  DirectChat,
+  Dropdown,
+  Layout,
+  PushMenu,
+  Toasts,
+  TodoList,
+  Treeview
+}
