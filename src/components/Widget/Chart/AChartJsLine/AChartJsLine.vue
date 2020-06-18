92be77163dd9855937b835d36331f0eb756ecc4a
DaviMenezes <davimenezes.dev@gmail.com>
2020-06-18 18:26:25 -0300

    file of the chartjs line component created

---
+++ src/components/Widget/Chart/AChartJsLine/AChartJsLine.vue
@@ -0,0 +1,74 @@
+<template>
+  <canvas id="lineChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+</template>
+
+<script>
+import $ from 'jquery'
+import Chart from 'chart.js'
+const jQuery = $
+export default {
+  props: {
+  },
+  mounted () {
+    const areaChartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Digital Goods',
+          backgroundColor: 'rgba(60,141,188,0.9)',
+          borderColor: 'rgba(60,141,188,0.8)',
+          pointRadius: false,
+          pointColor: '#3b8bba',
+          pointStrokeColor: 'rgba(60,141,188,1)',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data: [28, 48, 40, 19, 86, 27, 90]
+        },
+        {
+          label: 'Electronics',
+          backgroundColor: 'rgba(210, 214, 222, 1)',
+          borderColor: 'rgba(210, 214, 222, 1)',
+          pointRadius: false,
+          pointColor: 'rgba(210, 214, 222, 1)',
+          pointStrokeColor: '#c1c7d1',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    }
+    const areaChartOptions = {
+      maintainAspectRatio: false,
+      responsive: true,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: false
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            display: false
+          }
+        }]
+      }
+    }
+    const lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+    const lineChartOptions = jQuery.extend(true, {}, areaChartOptions)
+    const lineChartData = jQuery.extend(true, {}, areaChartData)
+    lineChartData.datasets[0].fill = false
+    lineChartData.datasets[1].fill = false
+    lineChartOptions.datasetFill = false
+
+    // eslint-disable-next-line no-new
+    new Chart(lineChartCanvas, {
+      type: 'line',
+      data: lineChartData,
+      options: lineChartOptions
+    })
+  }
+}
+</script>
