7d8455eb1f36597823001402ace879797452a954
DaviMenezes <davimenezes.dev@gmail.com>
2020-07-02 09:32:19 -0300

    refactorings

--- src/views/themes/adminlte-vue/Demo/Pages/Dashboards/3/ACardOnlineStoreOverview.vue
+++ src/views/themes/adminlte-vue/Demo/Pages/Dashboards/3/ACardOnlineStoreOverview.vue
@@ -1,68 +1,65 @@
 <template>
-  <Card>
-    <template slot="header">
-      <CardHeader title="Online Store Overview" class="border-0">
-        <template slot="tools">
-          <a href="#" class="btn btn-sm btn-tool">
-            <i class="fas fa-download"></i>
-          </a>
-          <a href="#" class="btn btn-sm btn-tool">
-            <i class="fas fa-bars"></i>
-          </a>
-        </template>
-      </CardHeader>
-      <CardBody>
-        <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-          <p class="text-success text-xl">
-            <i class="ion ion-ios-refresh-empty"></i>
-          </p>
-          <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-up text-success"></i> 12%
-                    </span>
-            <span class="text-muted">CONVERSION RATE</span>
-          </p>
-        </div>
-        <!-- /.d-flex -->
-        <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-          <p class="text-warning text-xl">
-            <i class="ion ion-ios-cart-outline"></i>
-          </p>
-          <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
-                    </span>
-            <span class="text-muted">SALES RATE</span>
-          </p>
-        </div>
-        <!-- /.d-flex -->
-        <div class="d-flex justify-content-between align-items-center mb-0">
-          <p class="text-danger text-xl">
-            <i class="ion ion-ios-people-outline"></i>
-          </p>
-          <p class="d-flex flex-column text-right">
-                    <span class="font-weight-bold">
-                      <i class="ion ion-android-arrow-down text-danger"></i> 1%
-                    </span>
-            <span class="text-muted">REGISTRATION RATE</span>
-          </p>
-        </div>
-      </CardBody>
-    </template>
-    <template slot="body"></template>
-  </Card>
+  <a-card>
+    <a-card-header title="Online Store Overview" class="border-0" slot="header">
+      <template slot="tools">
+        <a href="#" class="btn btn-sm btn-tool">
+          <i class="fas fa-download"></i>
+        </a>
+        <a href="#" class="btn btn-sm btn-tool">
+          <i class="fas fa-bars"></i>
+        </a>
+      </template>
+    </a-card-header>
+    <a-card-body>
+      <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+        <p class="text-success text-xl">
+          <i class="ion ion-ios-refresh-empty"></i>
+        </p>
+        <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-up text-success"></i> 12%
+                  </span>
+          <span class="text-muted">CONVERSION RATE</span>
+        </p>
+      </div>
+      <!-- /.d-flex -->
+      <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+        <p class="text-warning text-xl">
+          <i class="ion ion-ios-cart-outline"></i>
+        </p>
+        <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
+                  </span>
+          <span class="text-muted">SALES RATE</span>
+        </p>
+      </div>
+      <!-- /.d-flex -->
+      <div class="d-flex justify-content-between align-items-center mb-0">
+        <p class="text-danger text-xl">
+          <i class="ion ion-ios-people-outline"></i>
+        </p>
+        <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-down text-danger"></i> 1%
+                  </span>
+          <span class="text-muted">REGISTRATION RATE</span>
+        </p>
+      </div>
+    </a-card-body>
+  </a-card>
 </template>
 <script>
-import Card from '@/components/Widget/Card/ACard'
-import CardHeader from '@/components/Widget/Card/ACardHeader'
-import CardBody from '@/components/Widget/Card/ACardBody'
+import ACard from '@/components/Widget/Card/ACard'
+import ACardHeader from '@/components/Widget/Card/ACardHeader'
+import ACardBody from '@/components/Widget/Card/ACardBody'
 
 export default {
   name: 'ACardOnlineStoreOverview',
   components: {
-    Card,
-    CardHeader,
-    CardBody
+    ACard,
+    ACardHeader,
+    ACardBody
   },
   data () { return { } },
   props: { }
--- src/views/themes/adminlte-vue/Demo/Pages/Dashboards/3/ACardOnlineStoreVisitors.vue
+++ src/views/themes/adminlte-vue/Demo/Pages/Dashboards/3/ACardOnlineStoreVisitors.vue
@@ -1,52 +1,50 @@
 <template>
-  <Card>
+  <a-card>
     <template slot="header">
-      <CardHeader class="border-0">
+      <a-card-header class="border-0">
         <template>
           <div class="d-flex justify-content-between">
             <h3 class="card-title">Online Store Visitors</h3>
             <a href="javascript:void(0);">View Report</a>
           </div>
         </template>
-      </CardHeader>
+      </a-card-header>
     </template>
-    <template slot="body">
-      <CardBody>
-        <div class="d-flex">
-          <p class="d-flex flex-column">
-            <span class="text-bold text-lg">820</span>
-            <span>Visitors Over Time</span>
-          </p>
-          <p class="ml-auto d-flex flex-column text-right">
-            <span class="text-success">
-              <i class="fas fa-arrow-up"></i> 12.5%
-            </span>
-            <span class="text-muted">Since last week</span>
-          </p>
-        </div>
-        <!-- /.d-flex -->
+    <a-card-body>
+      <div class="d-flex">
+        <p class="d-flex flex-column">
+          <span class="text-bold text-lg">820</span>
+          <span>Visitors Over Time</span>
+        </p>
+        <p class="ml-auto d-flex flex-column text-right">
+          <span class="text-success">
+            <i class="fas fa-arrow-up"></i> 12.5%
+          </span>
+          <span class="text-muted">Since last week</span>
+        </p>
+      </div>
+      <!-- /.d-flex -->
 
-        <div class="position-relative mb-4">
-          <canvas id="visitors-chart" height="200"></canvas>
-        </div>
+      <div class="position-relative mb-4">
+        <canvas id="visitors-chart" height="200"></canvas>
+      </div>
 
-        <div class="d-flex flex-row justify-content-end">
-                  <span class="mr-2">
-                    <i class="fas fa-square text-primary"></i> This Week
-                  </span>
+      <div class="d-flex flex-row justify-content-end">
+                <span class="mr-2">
+                  <i class="fas fa-square text-primary"></i> This Week
+                </span>
 
-          <span>
-                    <i class="fas fa-square text-gray"></i> Last Week
-                  </span>
-        </div>
-      </CardBody>
-    </template>
-  </Card>
+        <span>
+                  <i class="fas fa-square text-gray"></i> Last Week
+                </span>
+      </div>
+    </a-card-body>
+  </a-card>
 </template>
 <script>
-import Card from '@/components/Widget/Card/ACard'
-import CardHeader from '@/components/Widget/Card/ACardHeader'
-import CardBody from '@/components/Widget/Card/ACardBody'
+import ACard from '@/components/Widget/Card/ACard'
+import ACardHeader from '@/components/Widget/Card/ACardHeader'
+import ACardBody from '@/components/Widget/Card/ACardBody'
 
 import $ from 'jquery'
 import Chart from 'chart.js'
@@ -54,9 +52,9 @@ import Chart from 'chart.js'
 export default {
   name: 'ACardOnlineStoreVisitors',
   components: {
-    Card,
-    CardHeader,
-    CardBody
+    ACard,
+    ACardHeader,
+    ACardBody
   },
   data () {
     return {}
